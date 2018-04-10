14749926ffa48a9d3b5f700bcdc9f55071040e40
szheng20 <Dongcidacidian233>
2018-04-09 19:27:08 -0500

    Initial commit

---
+++ src/Achievements.java
@@ -0,0 +1,10 @@
+public class Achievements {
+    public int player;
+    public int[] achievements;
+
+    public Achievements(int player, String[] lists) {
+        super();
+        this.player = player;
+    }
+}
+//this.lists = lists;
---
+++ src/Game.java
@@ -0,0 +1,11 @@
+public class Game {
+    public int player;
+    public int[] achievements;
+    public int score;
+    public int playtimes;
+
+
+    public Game(int player){
+
+    }
+}
